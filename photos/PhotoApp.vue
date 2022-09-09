4c4a316658b899693769bde9bcf68759e6a93464
jlazim <jlazim@deloitte.com>
2022-09-09 18:48:47 +0800

    added photos featurelearned routerused cache

---
+++ photos/PhotoApp.vue
@@ -0,0 +1,57 @@
+<template>
+    <layout>
+        <template v-slot:header>
+            Header
+        </template>
+        <template v-slot:sidebar>
+            <album
+                v-for="album in albums"
+                :key="album.id"    
+                :album="album"
+            />
+        </template>
+        <template v-slot:content>
+            <router-view />
+        </template>
+    </layout>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import Layout from './Layout.vue'
+import Album from './Album.vue'
+
+export default  {
+    components: {
+        Layout,
+        Album
+    },
+
+    setup() {
+        const store = useStore()
+        
+        onMounted(() => {
+            store.dispatch('albums/fetch')
+        })
+        
+        const albums = computed(() => {
+            return store.state.albums.all
+        })
+
+        return {
+            albums
+        }
+    },
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+}
+</style>
